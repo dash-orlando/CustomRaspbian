c18976ebb9bb03bc45cfe781a0e37ebdd9a7f943
moe93 <m.odeh93@gmail.com>
2018-04-26 11:28:14 -0400

    [BETA] script to create an AP

---
+++ RPi3_WiFi_Hotspot.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+# Use RPi3 as an internet AP and Wi-Fi hotspot
+#
+# USAGE:
+# ./rPi3-ap-setup.sh <password> <ssid>
+#
+
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root"
+	exit
+fi
+
+if [[ $# -lt 1 ]]; 
+	then echo "You need to pass a password!"
+	echo "Usage:"
+	echo "sudo $0 yourChosenPassword [apName]"
+	exit
+fi
+
+APPASS="$1"
+APSSID="rPi3"
+
+if [[ $# -eq 2 ]]; then
+	APSSID=$2
+fi
+
+apt-get remove --purge hostapd -yqq
+apt-get update -yqq
+apt-get upgrade -yqq
+apt-get install hostapd dnsmasq -yqq
+
+cat > /etc/dnsmasq.conf <<EOF
+interface=wlan0
+dhcp-range=10.0.0.2,10.0.0.5,255.255.255.0,12h
+EOF
+
+cat > /etc/hostapd/hostapd.conf <<EOF
+interface=wlan0
+hw_mode=g
+channel=10
+auth_algs=1
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
+rsn_pairwise=CCMP
+wpa_passphrase=$APPASS
+ssid=$APSSID
+ieee80211n=1
+wmm_enabled=1
+ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
+EOF
+
+sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
+sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
+sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
+sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
+
+cat >> /etc/network/interfaces <<EOF
+# Added by rPi Access Point Setup
+allow-hotplug wlan0
+iface wlan0 inet static
+	address 10.0.0.1
+	netmask 255.255.255.0
+	network 10.0.0.0
+	broadcast 10.0.0.255
+
+EOF
+
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+
+systemctl enable hostapd
+systemctl enable dnsmasq
+
+sudo service hostapd start
+sudo service dnsmasq start
+
+echo "All done! Please reboot"
+
+# --------------------------------------------------
+
+ADAPTER="eth0"
+
+## Allow overriding from eth0 by passing in a single argument
+#if [ $# -eq 1 ]; then
+#    ADAPTER="$1"
+#fi
+
+#Uncomment net.ipv4.ip_forward
+sed -i -- 's/#net.ipv4.ip_forward/net.ipv4.ip_forward/g' /etc/sysctl.conf
+#Change value of net.ipv4.ip_forward if not already 1
+sed -i -- 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+#Activate on current system
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -t nat -A POSTROUTING -o $ADAPTER -j MASQUERADE  
+iptables -A FORWARD -i $ADAPTER -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+iptables -A FORWARD -i wlan0 -o $ADAPTER -j ACCEPT
+
+# Make sure these rules are applied everytime we reboot the RPi
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+
+sed -i -e 's/exit 0/iptables-restore < /etc/iptables.ipv4.nat/g' /etc/rc.local
